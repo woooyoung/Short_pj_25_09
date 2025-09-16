78aa17dba3967a71bfecf57a75047f080684bb6f
woooyoung <axdsw121@gmail.com>
2025-09-16 12:31:05 +0900

    단축방식 설정, 원본 URL로 이동

--- src/main/java/com/koreait/short_pj_25_09/ShortUrlController.java
+++ src/main/java/com/koreait/short_pj_25_09/ShortUrlController.java
@@ -1,10 +1,13 @@
 package com.koreait.short_pj_25_09;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -25,5 +28,56 @@ public class ShortUrlController {
         surls.add(surl);
 
         return surl;
+    }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public Surl add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+        String url = req.getRequestURI();
+
+        if(req.getQueryString() != null) {
+            url += "?" + req.getQueryString();
+        }
+
+        String[] urlBits = url.split("/",4);
+
+        System.out.println("Arrays.toString(urlBits): " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
+        Surl surl = Surl.builder()
+                .id(++surlLastId)
+                .body(body)
+                .url(url)
+                .build();
+
+        surls.add(surl);
+
+        return surl;
+    }
+
+    @GetMapping("/g/{id}")
+    public String go(
+            @PathVariable long id
+    ){
+        Surl surl = surls.stream()
+                .filter(_surl -> _surl.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if(surl == null) throw new RuntimeException("%d번 데이터 없음".formatted(id));
+
+        surl.increaseCount();
+
+        return "redirect:"+surl.getUrl();
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Surl> getAll(){
+        return surls;
     }
 }
--- src/main/java/com/koreait/short_pj_25_09/Surl.java
+++ src/main/java/com/koreait/short_pj_25_09/Surl.java
@@ -1,5 +1,6 @@
 package com.koreait.short_pj_25_09;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,4 +18,11 @@ public class Surl {
     private LocalDateTime modifyDate = LocalDateTime.now();
     private String body;
     private String url;
+
+    @Setter(AccessLevel.NONE)
+    private long count;
+
+    public void increaseCount() {
+        count++;
+    }
 }
